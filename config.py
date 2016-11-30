48be6a7ac2f5d2cd1f8b3515fd3c4684de185f6c
Chaitanya Vardhan <chaitanyavardhan1@gmail.com>
2016-11-30 18:21:52 +0530

    11d blog post pagination

--- config.py
+++ config.py
@@ -8,6 +8,7 @@ class Config:
 	FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
 	FLASKY_MAIL_SENDER = 'Flask Admin <chaitanyavardhan1@gmail.com>'
 	FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
+	FLASKY_POSTS_PER_PAGE = 30
 
 	@staticmethod
 	def init_app(app):
